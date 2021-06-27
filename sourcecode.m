52a21d337d2ea52866b47f8756d8187ac8ea96f2
Hamdi Boukamcha <contact@matlab-recognition-code.com>
2021-06-27 22:29:56 +0200

    Update sourcecode.m

--- sourcecode.m
+++ sourcecode.m
@@ -7,12 +7,6 @@
 % 
 % For more information please email me hamdouchhd@hotmail.com
 %
-% Hamdi Boukamcha
-% Sousse
-% 4081
-% Tunisia
-% email  hamdouchhd@hotmail.com
-% mobile +21650674269
 % website http://matlab-recognition-code.com
 %
 %
